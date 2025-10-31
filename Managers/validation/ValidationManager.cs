582ceb2f0190e017f31e10314d0ebd6032a04109
patryk <random12@onet.pl>
2025-10-31 17:25:13 +0100

    added global error handler and validation filter

---
+++ Managers/validation/ValidationManager.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Managers.validation
+{
+    public class ValidationManager
+    {
+        public Dictionary<string, string[]>? FormatErrors(ModelStateDictionary modelState)
+        {
+            var result = modelState.Where(x => x.Value.Errors.Count > 0)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Errors.Select(x => x.ErrorMessage).ToArray());
+            return result;
+        }
+    }
+}
