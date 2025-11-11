f3d2be1138138fde93f265bda3550b0b0cb60682
patryk <random12@onet.pl>
2025-11-11 14:38:30 +0100

    wip

---
+++ Models/firebase/FirebaseDeviceMeasurement.cs
@@ -0,0 +1,16 @@
+﻿using Google.Cloud.Firestore;
+
+namespace Models.firebase
+{
+    public class FirebaseDeviceMeasurement
+    {
+        [FirestoreDocumentId]
+        public string Id { get; set; }
+        [FirestoreProperty("serialNumber")]
+        public string SerialNumber { get; set; }
+        [FirestoreProperty("timestamp")]
+        public long Timestamp { get; set; }
+        [FirestoreProperty("parameters")]
+        public Dictionary<string, string> Parameters { get; set; }
+    }
+}
