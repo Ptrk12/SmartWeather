6ce735b35ee190b3cfe54c664d70500630d84f32
patryk <random12@onet.pl>
2025-11-15 21:23:36 +0100

    added add sensor metric endpoint

---
+++ Models/requests/CreateSensorMetric.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.requests
+{
+    public class CreateSensorMetric
+    {
+        [Required]
+        public string SensorType { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Unit { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}
